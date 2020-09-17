532db51e93bade478ad714dab1beec2e011c3d2a
Ayat Rashid <ayatrashid@Ayats-MacBook-Pro.local>
2020-09-17 01:19:36 -0700

    create node server

---
+++ frontend/src/dataArray.js
@@ -0,0 +1,77 @@
+export default {
+    company: "ayat co",
+    products: [
+        {
+        id: '1',
+        image:'images/marval.jpg',
+        name: 'T-shirt1',
+        price:'$20',
+        logo:'marval',
+        numReview:'10'
+        },
+        {
+        id: '2',   
+        image:'./images/eye.jpg',
+        name: 'T-shirt2',
+        price:'$20',
+        logo:'marval',
+        numReview:'10'
+        },
+        {
+        id: '3',
+        image:'./images/marval.jpg',
+        name: 'T-shirt3',
+        price:'$20',
+        logo:'marval',
+        numReview:'10'
+        },
+        {
+        id: '4',
+        image:'./images/marval.jpg',
+        name: 'T-shirt',
+        price:'$20',
+        logo:'marval',
+        numReview:'10'
+        },
+        {
+        id: '5',
+        image:'./images/marval.jpg',
+        name: 'T-shirt',
+        price:'$20',
+        logo:'marval',
+        numReview:'10'
+        },
+        {
+        id: '6',
+        image:'./images/marval.jpg',
+        name: 'T-shirt',
+        price:'$20',
+        logo:'marval',
+        numReview:'10'
+        },
+        {
+        id: '7',
+        image:'./images/marval.jpg',
+        name: 'T-shirt',
+        price:'$20',
+        logo:'marval',
+        numReview:'10'
+        },
+        {
+        id: '8',
+        image:'./images/marval.jpg',
+        name: 'T-shirt',
+        price:'$20',
+        logo:'marval',
+        numReview:'10'
+        },
+        {
+        id: '9',
+        image:'./images/marval.jpg',
+        name: 'T-shirt',
+        price:'$20',
+        logo:'marval',
+        numReview:'10'
+        }
+     ]
+}
